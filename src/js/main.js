4bb545a9bdbb93e18d4ea9cf1eb7c53301821d7e
Vuzeta95 <machnio95@o2.pl>
2019-10-23 22:06:18 +0200

    working on hamburger

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,10 @@
 import '../scss/main.scss';
-console.log('test');
+
+//Open hamburger
+
+const hamburger = document.querySelector('.navigation__hamburger--icon');
+const navigation = document.querySelector('.navigation__list');
+
+hamburger.addEventListener('click', () => {
+  navigation.style.top = '10%';
+});
