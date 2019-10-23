401720a179dc9762bc815690b9e4b3107036b234
Vuzeta <machniik95@gmail.com>
2019-10-23 16:44:24 +0200

    add url loader, font-awsome, working on nav

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,69 +4,77 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/main.js',
-  output: {
-    path: path.resolve(__dirname, '../', 'build'),
-    filename: 'js/[name].js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(sass|scss)$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
-              publicPath: '../images',
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-          },
-        ],
-      },
-      {
-        test: /\.(html)$/,
-        use: {
-          loader: 'html-loader',
-          options: {
-            attrs: [':src'],
-          },
-        },
-      },
-    ],
-  },
-  devServer: {
-    open: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'css/[name].css',
-    }),
-  ],
+	mode: 'development',
+	entry: './src/js/main.js',
+	output: {
+		path: path.resolve(__dirname, '../', 'build'),
+		filename: 'js/[name].js',
+		publicPath: '/',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.svg/,
+				use: {
+					loader: 'svg-url-loader',
+					options: {},
+				},
+			},
+			{
+				test: /\.(sass|scss)$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+			{
+				test: /\.(png|jpe?g|gif)$/i,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'images',
+							publicPath: '../images',
+						},
+					},
+					{
+						loader: 'image-webpack-loader',
+					},
+				],
+			},
+			{
+				test: /\.(html)$/,
+				use: {
+					loader: 'html-loader',
+					options: {
+						attrs: [':src'],
+					},
+				},
+			},
+		],
+	},
+	devServer: {
+		open: true,
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'css/[name].css',
+		}),
+	],
 };
