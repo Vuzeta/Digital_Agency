e697c84524c6447315e9c6d293999227623b05ce
Vuzeta <machniik95@gmail.com>
2019-10-25 16:01:34 +0200

    working on navigation(2)

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,14 @@
 import '../scss/main.scss';
 
 //HAMBURGER MENU
-const hamburger = document.querySelector('.navigation__hamburger--icon');
+const hamburger = document.querySelector('.navigation__hamburger');
 const navigation = document.querySelector('.navigation__list');
-const hamburgerIcon = document.querySelector('.navigation__hamburger--icon');
 
 hamburger.addEventListener('click', () => {
-	if (hamburgerIcon.classList.contains('fa-bars'))
-		hamburgerIcon.classList.replace('fa-bars', 'fa-arrow-up');
-	else hamburgerIcon.classList.replace('fa-arrow-up', 'fa-bars');
+	console.log('click');
+	if (hamburger.classList.contains('fa-bars'))
+		hamburger.classList.replace('fa-bars', 'fa-arrow-up');
+	else hamburger.classList.replace('fa-arrow-up', 'fa-bars');
 
 	navigation.classList.toggle('navigation__list--open');
 });
