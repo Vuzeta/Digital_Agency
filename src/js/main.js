b83b8c55612c22ebd59e429d77e76f295b27c243
Vuzeta95 <machnio95@o2.pl>
2019-11-01 16:06:43 +0100

    add smooth scroll, remove comments

--- src/js/main.js
+++ src/js/main.js
@@ -4,12 +4,24 @@ import animationProgress from './animationProgress';
 import fixedNavigation from './fixedNavigation';
 import slideNavigation from './slideNavigation';
 import hamburger from './hamburger';
+import jump from 'jump.js';
 
-//HAMBURGER MENU
 const hamburgerElement = document.querySelector('.navigation__hamburger');
+const navLinks = document.querySelectorAll('.navigation__link');
 
 hamburgerElement.addEventListener('click', () => hamburger(hamburgerElement));
 window.addEventListener('load', () => slideNavigation());
 document.addEventListener('scroll', () => animationFacts());
 document.addEventListener('scroll', () => animationProgress());
 document.addEventListener('scroll', () => fixedNavigation());
+navLinks.forEach(link =>
+  link.addEventListener('click', function(e) {
+    jump(`${e.target.hash}`, {
+      duration: 1000,
+      offset: -110,
+    });
+    if (window.screen.availWidth < 641) {
+      hamburger(hamburgerElement);
+    }
+  }),
+);
